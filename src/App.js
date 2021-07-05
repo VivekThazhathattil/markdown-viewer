23619ca366cb9ae16ffd650bad3e80d2852dee64
Vivek Thazhathattil <vivek.thazhathattil@gmail.com>
2021-07-05 17:00:02 +0530

    add placeholder markdown and html output

--- src/App.js
+++ src/App.js
@@ -4,6 +4,50 @@ import React from 'react';
 import marked from 'marked';
 import GithubCorner from 'react-github-corner';
 
+const placeholder = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![pillar](https://mirrors.creativecommons.org/presskit/icons/cc.png)
+`;
 const MainTitle = () =>{
 	return(
 		<h1 className="auto-margin big-font">Markdown Previewer</h1>
@@ -28,7 +72,7 @@ class App extends React.Component{
 		super(props);
 		this.state ={
 			input: "",
-			markdown: []
+			markdown: marked(placeholder)
 		}
 		this.inputChange = this.inputChange.bind(this);
 	}
@@ -51,6 +95,7 @@ class App extends React.Component{
 							<textarea 
 								className="fill-area margin10"
 								type="text"
+								placeholder={placeholder}
 								onChange={this.inputChange}>
 							</textarea>
 						</div>
